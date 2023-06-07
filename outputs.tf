7eb17035fc9de643796a670c90e0c621586f894d
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-06-07 12:05:06 -0500

    adding regional domain name as output var

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "arn" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#arn"
   value       = aws_s3_bucket.this.arn
 }
+
+output "bucket_regional_domain_name" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#bucket_regional_domain_name"
+  value       = aws_s3_bucket.this.bucket_regional_domain_name
+}
