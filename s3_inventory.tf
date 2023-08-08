708f0e1926d73e075fcc881b6c64ec809e0db58a
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-08-08 13:14:46 -0500

    testing inventory s3 process

--- s3_inventory.tf
+++ s3_inventory.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket_inventory" "this" {
   count  = var.enable_s3_inventory ? 1 : 0
   bucket = aws_s3_bucket.this.id
-  name   = var.use_name_prefix ? aws_s3_bucket.this.bucket_prefix : aws_s3_bucket.this.bucket
+  name   = "inventory"
 
   included_object_versions = "All"
 
