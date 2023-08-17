97d86a16cb5df682823b5f61be2ea3e549500bfd
Justin Reeves <reeves122@gmail.com>
2023-08-17 14:22:00 -0500

    add for loop to check for replicated file

--- examples/replication/test.sh
+++ examples/replication/test.sh
@@ -1,6 +1,21 @@
 echo test > test.txt
 aws s3 cp test.txt s3://$SOURCE_BUCKET/test.txt
 
-sleep 5
+for i in {1..10}; do
+  aws s3 ls s3://$DESTINATION_BUCKET | grep test.txt
+  result=$?
+  if [[ $result -eq 0 ]]
+  then
+    echo "found replicated file"
+    break
+  else
+    echo "replicated file not found"
+    sleep 3
+  fi
+done
 
-aws s3 ls s3://$DESTINATION_BUCKET
+if [[ $result -ne 0 ]]
+then
+  echo "the file was not replicated"
+  exit 1
+fi
