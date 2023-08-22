aa0c4ea6ed9cc58d66b07eef861bf4254cea313e
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-08-22 07:57:21 -0500

    output id

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "bucket_regional_domain_name" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#bucket_regional_domain_name"
   value       = aws_s3_bucket.this.bucket_regional_domain_name
 }
+
+output "id" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#id"
+  value       = aws_s3_bucket.this.id
+}
