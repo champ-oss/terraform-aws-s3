fe8ecc2288b567da27b795e4f7bd9d1b17f1e7b8
Justin Reeves <reeves122@gmail.com>
2023-09-19 11:49:44 -0500

    use random name for test bucket

--- examples/custom_policy/main.tf
+++ examples/custom_policy/main.tf
@@ -16,8 +16,6 @@ data "aws_iam_policy_document" "this" {
 module "this" {
   source               = "../../"
   git                  = "terraform-aws-s3"
-  name                 = "test"
-  use_name_prefix      = false
   protect              = false
   enable_custom_policy = true
   policy               = data.aws_iam_policy_document.this.json
