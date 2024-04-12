a223dafea2bb40d50f6fba20b874e9398f63f341
Justin Reeves <reeves122@gmail.com>
2024-04-12 15:36:08 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -5,14 +5,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.20.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.6.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.0.0"
-    }
   }
 }
 
