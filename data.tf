7918e4ddbf34620337011c77f8fb4c5dd3fa4c2b
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-10 13:54:22 -0500

    testing with enabled set to false

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 data "aws_iam_policy_document" "combined" {
-  count = var.enable_custom_policy || var.enable_lb_policy || length(var.aws_cross_account_id_arns) != 0 && var.enabled ? 1 : 0
+  count = (var.enable_custom_policy || var.enable_lb_policy || length(var.aws_cross_account_id_arns) != 0) && var.enabled ? 1 : 0
   source_policy_documents = compact([
-    var.enable_lb_policy && var.enabled ? data.aws_iam_policy_document.lb[0].json : "",
-    var.enable_custom_policy && var.enabled ? var.policy : "",
-    length(var.aws_cross_account_id_arns) != 0 && var.enabled ? data.aws_iam_policy_document.cross_account[0].json : ""
+    var.enable_lb_policy ? data.aws_iam_policy_document.lb[0].json : "",
+    var.enable_custom_policy ? var.policy : "",
+    length(var.aws_cross_account_id_arns) != 0 ? data.aws_iam_policy_document.cross_account[0].json : ""
   ])
 }
 
