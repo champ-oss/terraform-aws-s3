a95b9844713e39ad9461d5b5edff945a5379f0f8
Phillip Langley <philliplangley@champmbplangley.lan>
2024-10-15 10:58:29 -0500

    left off rw off count

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "aws_iam_policy_document" "combined" {
-  count = (length(var.datasync_cross_account_id_arn) != 0 || var.enable_custom_policy || var.enable_lb_policy || length(var.aws_cross_account_id_arns) != 0) && var.enabled ? 1 : 0
+  count = (length(var.datasync_cross_account_id_arn) != 0 || length(var.s3_read_write_cross_account_id_arns) != 0 || var.enable_custom_policy || var.enable_lb_policy || length(var.aws_cross_account_id_arns) != 0) && var.enabled ? 1 : 0
   source_policy_documents = compact([
     var.enable_lb_policy ? data.aws_iam_policy_document.lb[0].json : "",
     var.enable_custom_policy ? var.policy : "",
